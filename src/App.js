e3f0d3df50ef58ff3ef26bb58f2bbbb2fe5e6ccf
Christine Gundel <gundelc@gmail.com>
2024-01-12 15:01:41 +0100

    Login page created
    The Login pages was created using react-router-dom 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//pages and components
+import Login from './pages/login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+       <Routes>
+        <Route path="/codeathome_final_project/login" element={<Login />} />
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
