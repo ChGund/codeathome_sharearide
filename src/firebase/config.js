986c02ca06a80693df0019ccef277d426688cfe8
Christine Gundel <gundelc@gmail.com>
2024-01-31 08:36:15 +0100

    Set up Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLb-9fxo02_8f7qIMzWsXuDOXAGIbzgT4",
+    authDomain: "share-a-ride-e4e6f.firebaseapp.com",
+    projectId: "share-a-ride-e4e6f",
+    storageBucket: "share-a-ride-e4e6f.appspot.com",
+    messagingSenderId: "372703946429",
+    appId: "1:372703946429:web:aa1004a23036003f82f9d4"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init service
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  export { projectFirestore, projectAuth }
