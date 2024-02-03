2b614ba9d6e9fb2976023d86c7e51016a962ef5b
Christine Gundel <gundelc@gmail.com>
2024-02-03 17:01:57 +0100

    Format documents

--- src/App.js
+++ src/App.js
@@ -1,52 +1,83 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 //pages and components
-import Login from './pages/login/Login'
-import Signin from './pages/signin/Signin'
-import Search from './pages/search/Search';
-import Share from './pages/share/Share';
-import About from './pages/about/About';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import Login from "./pages/login/Login";
+import Signin from "./pages/signin/Signin";
+import Search from "./pages/search/Search";
+import Share from "./pages/share/Share";
+import About from "./pages/about/About";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-import styles from './App.css'
-
+import styles from "./App.css";
 
 function App() {
-  const { authIsReady, user } = useAuthContext()
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className={styles.App}>
       {authIsReady && (
         <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route 
-              path="/codeathome_final_project/" 
-              element={user ? <Navigate to="/codeathome_final_project/search" /> : <Login />} />
-          <Route 
-              path="/codeathome_final_project/login" 
-              element={user ? <Navigate to="/codeathome_final_project/search" /> : <Login />} />
-          <Route 
-              path="/codeathome_final_project/signin" 
-              element={user ? <Navigate to="/codeathome_final_project/search" /> : <Signin />} />
-          <Route 
-              path="/codeathome_final_project/search" 
-              element={!user ? <Navigate to="/codeathome_final_project/login" /> : <Search />} />
-          <Route 
-              path="/codeathome_final_project/share" 
-              element={!user ? <Navigate to="/codeathome_final_project/login" /> : <Share />} /> 
-          <Route 
-              path="/codeathome_final_project/about" 
-              element={<About />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/codeathome_final_project/"
+              element={
+                user ? (
+                  <Navigate to="/codeathome_final_project/search" />
+                ) : (
+                  <Login />
+                )
+              }
+            />
+            <Route
+              path="/codeathome_final_project/login"
+              element={
+                user ? (
+                  <Navigate to="/codeathome_final_project/search" />
+                ) : (
+                  <Login />
+                )
+              }
+            />
+            <Route
+              path="/codeathome_final_project/signin"
+              element={
+                user ? (
+                  <Navigate to="/codeathome_final_project/search" />
+                ) : (
+                  <Signin />
+                )
+              }
+            />
+            <Route
+              path="/codeathome_final_project/search"
+              element={
+                !user ? (
+                  <Navigate to="/codeathome_final_project/login" />
+                ) : (
+                  <Search />
+                )
+              }
+            />
+            <Route
+              path="/codeathome_final_project/share"
+              element={
+                !user ? (
+                  <Navigate to="/codeathome_final_project/login" />
+                ) : (
+                  <Share />
+                )
+              }
+            />
+            <Route path="/codeathome_final_project/about" element={<About />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       )}
     </div>
   );
 }
 
 export default App;
-
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,33 +1,34 @@
-import { useCollection } from '../../hooks/useCollection';
+import { useCollection } from "../../hooks/useCollection";
 
-import styles from '../share/Share.module.css'
+import styles from "../share/Share.module.css";
 
 export default function Search() {
-  const { documents: rides, error } = useCollection('offeredRides');
-  
+  const { documents: rides, error } = useCollection("offeredRides");
+
   return (
     <>
-    <h2>Search a Ride:</h2>
-    <div className={styles['container-search']}> 
-    <div></div>
-      <ul className={styles['rides-list']}>
-        {rides && rides
-          .slice() 
-          .sort((a, b) => new Date(a.date) - new Date(b.date)) 
-          .map((ride) => (
-            <li key={ride.id}>
-              <p className={styles.location}>{ride.location}</p>
-              <p className={styles.arrow}>>>></p>
-              <p className={styles.destination}>{ride.destination}</p>
-              <p className={styles.date}>{ride.date}</p>   
-              <p className={styles.seats}>Seats: {ride.seats}</p>
-              <p className={styles.contact}>Contact: {ride.contact}</p>
-            </li>
-          ))}
+      <h2>Search a Ride:</h2>
+      <div className={styles["container-search"]}>
+        <div></div>
+        <ul className={styles["rides-list"]}>
+          {rides &&
+            rides
+              .slice()
+              .sort((a, b) => new Date(a.date) - new Date(b.date))
+              .map((ride) => (
+                <li key={ride.id}>
+                  <p className={styles.location}>{ride.location}</p>
+                  <p className={styles.arrow}>>>></p>
+                  <p className={styles.destination}>{ride.destination}</p>
+                  <p className={styles.date}>{ride.date}</p>
+                  <p className={styles.seats}>Seats: {ride.seats}</p>
+                  <p className={styles.contact}>Contact: {ride.contact}</p>
+                </li>
+              ))}
           <div></div>
-      </ul>
-      {error && <p>{error}</p>}
-    </div>
+        </ul>
+        {error && <p>{error}</p>}
+      </div>
     </>
   );
 }
